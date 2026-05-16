7a70e85dbe8722812a6b42a8c89b092126a427fa
Helmer <helmer@zandbergenonline.nl>
2026-05-16 19:06:20 +0200

    fix: bump SW to v13 to clear cached scanner.js with resultPlot ref

--- app/frontend/service-worker.js
+++ app/frontend/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "discvault-sw-v12";
+const SW_VERSION = "discvault-sw-v13";
 const APP_CACHE = `${SW_VERSION}-app`;
 const API_CACHE = `${SW_VERSION}-api`;
 const RUNTIME_CACHE = `${SW_VERSION}-runtime`;
