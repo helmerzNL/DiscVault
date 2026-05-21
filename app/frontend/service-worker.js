92dc4b9a9c8902244185315f3a06af96dbe52582
Helmer <helmer@zandbergenonline.nl>
2026-05-21 12:05:08 +0200

    fix: select width max-content + appearance:none, SW v101 cache bust

--- app/frontend/service-worker.js
+++ app/frontend/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "discvault-sw-v100";
+const SW_VERSION = "discvault-sw-v101";
 const APP_CACHE = `${SW_VERSION}-app`;
 const API_CACHE = `${SW_VERSION}-api`;
 const RUNTIME_CACHE = `${SW_VERSION}-runtime`;
