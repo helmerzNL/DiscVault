abb6a1ff15357f9165cd48ab0457101242efb37e
Helmer <helmer@zandbergenonline.nl>
2026-05-25 23:27:28 +0200

    Hide movie save action for box set scans

--- app/frontend/service-worker.js
+++ app/frontend/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "discvault-sw-v125";
+const SW_VERSION = "discvault-sw-v126";
 const APP_CACHE = `${SW_VERSION}-app`;
 const API_CACHE = `${SW_VERSION}-api`;
 const RUNTIME_CACHE = `${SW_VERSION}-runtime`;
