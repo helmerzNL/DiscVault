15c849b0cd04c00b5684e30e9449bb914b0d5881
Helmer <helmer@zandbergenonline.nl>
2026-05-25 22:22:04 +0200

    Fix movie poster selection

--- app/frontend/service-worker.js
+++ app/frontend/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "discvault-sw-v122";
+const SW_VERSION = "discvault-sw-v123";
 const APP_CACHE = `${SW_VERSION}-app`;
 const API_CACHE = `${SW_VERSION}-api`;
 const RUNTIME_CACHE = `${SW_VERSION}-runtime`;
