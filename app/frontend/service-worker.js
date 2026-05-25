d1fb6ca251f2285c5fc52d5b7c1066570d4e2508
Helmer <helmer@zandbergenonline.nl>
2026-05-25 22:59:06 +0200

    Fix edit media action handling

--- app/frontend/service-worker.js
+++ app/frontend/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "discvault-sw-v124";
+const SW_VERSION = "discvault-sw-v125";
 const APP_CACHE = `${SW_VERSION}-app`;
 const API_CACHE = `${SW_VERSION}-api`;
 const RUNTIME_CACHE = `${SW_VERSION}-runtime`;
