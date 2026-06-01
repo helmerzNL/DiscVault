11d18a254a1e59c9210fdc6d1452412d84ba581a
Helmer <helmer@zandbergenonline.nl>
2026-06-01 23:24:41 +0200

    Support MovieVault 26 connection controls

--- app/frontend/service-worker.js
+++ app/frontend/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "discvault-sw-v134";
+const SW_VERSION = "discvault-sw-v135";
 const APP_CACHE = `${SW_VERSION}-app`;
 const API_CACHE = `${SW_VERSION}-api`;
 const RUNTIME_CACHE = `${SW_VERSION}-runtime`;
