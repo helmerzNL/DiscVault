86b8fcb90f5aec8474c2e3246081b22082e0566d
Helmer <helmer@zandbergenonline.nl>
2026-06-02 00:12:42 +0200

    Improve clean startup owner setup

--- app/frontend/service-worker.js
+++ app/frontend/service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "discvault-sw-v135";
+const SW_VERSION = "discvault-sw-v136";
 const APP_CACHE = `${SW_VERSION}-app`;
 const API_CACHE = `${SW_VERSION}-api`;
 const RUNTIME_CACHE = `${SW_VERSION}-runtime`;
